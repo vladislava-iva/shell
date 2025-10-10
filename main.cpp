c7d6541a59bb7e030d17b3a9f909c51780a455ae
vladislava-iva <vladislava.ivanova109@gmail.com>
2025-10-10 10:40:03 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
 #include  <iostream>
 #include <string>
+#include <fstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+  std::string history_file=std::string(getenv("home"))+"/.kubsh_history";
+  std::ofstream history_out(history_file, std::ios::app);
   std::string input;
+  std::cout<<"$ ";
 
-  while (true){
-	if (!std::getline(std::cin, input)) {	
-		break;
-        }
-
-	if (input.empty()){
-		continue;
-	}
-
+  while (std::getline(std::cin,input)){
 	if (input=="\\q") {
         break;
     }
 
-	std::cout<<input<<std::endl;
+    if (input=="history") {
+        std::ifstream history_in(history_file);
+        std:: string line;
+        while (std::getline(history_in,line)) {
+            std::cout<<line<<std::endl;
+        }
+
+        continue;
+    }
+
+    if (input.find("echo ")==0){
+        std::cout<<input.substr(5)<<std::endl;
+    } 
+
+    else if(!input.empty()){
+        std::cout<<input<<std::endl;
+    } 
+
+    if (!input.empty()){
+        history_out<<input<<std::endl;
+    }
   }
+
   return 0;
 }
